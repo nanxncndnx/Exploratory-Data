89f7ae33bd611c5a13b008704f41f0bd5e76a4c9
nanxncndnx <machiavelli.co4@gmail.com>
2024-06-06 18:45:49 -0400

    Adding streamlit and create a option menu for better accessibility to each parts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from dotenv import load_dotenv
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
@@ -10,6 +11,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import math
 from datetime import *
 import statistics as stats
+
+load_dotenv()
+Json = os.getenv('Json_File')
+import streamlit as st 
+from streamlit_option_menu import option_menu
+import altair as alt
 
 # PySpark modules =>
 import pyspark
@@ -22,6 +29,12 @@ from pyspark.sql import functions
 from pyspark.sql.types import *
 from pyspark.sql.functions import lit, desc, col, size, array_contains\
 , isnan, udf, hour, array_min, array_max, countDistinct
+
+from DetectMissingValues import DMV
+from PysparkLazyEvaluation import PLE
+from PysparkUdfRegistering import PUR
+from UnstackPysparkDataframe import UPD
+from RDD import RDD
 
 MAX_MEMORY = '15G'
 # Initialize a spark session =>
@@ -43,14 +56,13 @@ def init_spark():
     return spark
 
 spark = init_spark()
-data = './endomondoHR.json'
+data = Json
 
 # Load the main data set into pyspark data frame =>
 df = spark.read.json(data, mode="DROPMALFORMED")
-print('Data frame type: ' + str(type(df)))
+#print('Data frame type: ' + str(type(df)))
 
 # Overview of Dataset =>
-
 #print('Data overview')
 #df.printSchema()
 
@@ -63,5 +75,34 @@ print('Data frame type: ' + str(type(df)))
 #print(f'There are total {df.count()} row, Let print first 2 data rows:')
 #df.limit(2).toPandas()
 
-################################################################################
+alt.themes.enable("dark")
 
+#as you see this is our DashBoard =>
+with st.sidebar:
+    selected = option_menu("Features", ['Docs', 'Detect Missing Values', 'Pyspark Lazy Evaluation', 'Unstack Pyspark Dataframe', 'Pyspark UDF Registering', 'RDD'], 
+        icons=['file-text', 'bar-chart-fill', 'bar-chart-fill', 'bar-chart-fill', 'bar-chart-fill', 'bar-chart-fill'], default_index=0,
+            styles={
+    "container": {"padding": "important", "background-color": "#0F161E", "border-radius" : "10px"},
+    "icon": {"color": "white", "font-size": "20px"}, 
+    "nav-link": {"font-size": "20px", "text-align": "center", "margin":"15px", "--hover-color": "#9BE8DA", "border-radius" : "10px"},
+    "nav-link-selected": {"background-color": "#4F776F"},
+    }
+)
+
+if selected == "Docs":
+    st.subheader(":green[Hello] From Docs")
+
+elif selected == "Detect Missing Values":
+    DMV.create_page(df)
+
+elif selected == "Pyspark Lazy Evaluation":
+    PLE.create_page()
+
+elif selected == "Unstack Pyspark Dataframe":
+    UPD.create_page()
+
+elif selected == "Pyspark UDF Registering":
+    PUR.create_page()
+
+elif selected == "RDD":
+    RDD.create_page() 
